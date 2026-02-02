4988c95cdab4cc52c039e83bc245f88e87d6177e
mene0903 <cude0903@naver.com>
2026-02-02 18:44:45 +0900

    first commit

---
+++ src/main/java/com/handwriting/handwriting/HandwritingApplication.java
@@ -0,0 +1,13 @@
+package com.handwriting.handwriting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandwritingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandwritingApplication.class, args);
+	}
+
+}
